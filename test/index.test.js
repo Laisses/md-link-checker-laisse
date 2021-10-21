435d2346ff417dabce6a3c00efe414f53c1c9f75
Laisses <71999249+Laisses@users.noreply.github.com>
2021-10-21 12:16:19 -0300

    Altera o caminho do teste para relativo.

--- test/index.test.js
+++ test/index.test.js
@@ -1,4 +1,5 @@
 const pegaArquivo = require("../index");
+const path = require("path");
 
 const arrayResult = [
 	{
@@ -12,18 +13,18 @@ describe("pegaArquivo::", () => {
 	});
 
 	it("deve retornar array com resultados", async () => {
-		const result = await pegaArquivo("C:/Users/Laísse/Dropbox/Desenvolvimento/Alura/Cursos/Nodejs-primeira-biblioteca/test/arquivos/texto1.md");
+		const result = await pegaArquivo(path.join(__dirname, "arquivos/texto1.md"));
 		expect(result).toEqual(arrayResult);
 	});
 
 	it("deve retornar mensagem 'não há links'", async () => {
-		const result = await pegaArquivo("C:/Users/Laísse/Dropbox/Desenvolvimento/Alura/Cursos/Nodejs-primeira-biblioteca/test/arquivos/texto1_sem_links.md");
+		const result = await pegaArquivo(path.join(__dirname,"arquivos/texto1_sem_links.md"));
 		expect(result).toBe("Não há links nesse texto.");
 	});
 
 	it("deve lançar um erro na falta de arquivo", () => {
 		const capturaErro = async () => {
-			await pegaArquivo("C:/Users/Laísse/Dropbox/Desenvolvimento/Alura/Cursos/Nodejs-primeira-biblioteca/test/arquivos/");
+			await pegaArquivo(path.join(__dirname, "arquivos/"));
 			expect(capturaErro).toThrowError(/Não há arquivo no caminho/);
 		}
 	});
